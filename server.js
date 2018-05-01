bf15c1a81261f73090842ba9bd3ef25a5d50d413
ymcat1234 <z84146666@126.com>
2018-05-02 00:50:03 +0800

    minor change

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express');
 var kutil = require('./util/kutil.js');
 var wechat = require('wechat');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
+
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
